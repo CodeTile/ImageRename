e77286e5c3d4d64fc7f1a108484429f49072918d
BB <71fathoms@gmail.com>
2018-04-03 10:21:04 +0100

    Inital version of image rename projects

---
+++ ImageRename/frmMain.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ImageRename
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.btnFindPath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFileSummary = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Path";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtPath.Location = new System.Drawing.Point(108, 22);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(529, 20);
+            this.txtPath.TabIndex = 1;
+            // 
+            // btnFindPath
+            // 
+            this.btnFindPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnFindPath.Location = new System.Drawing.Point(643, 20);
+            this.btnFindPath.Name = "btnFindPath";
+            this.btnFindPath.Size = new System.Drawing.Size(29, 23);
+            this.btnFindPath.TabIndex = 2;
+            this.btnFindPath.Text = "...";
+            this.btnFindPath.UseVisualStyleBackColor = true;
+            this.btnFindPath.Click += new System.EventHandler(this.btnFindPath_Click);
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Location = new System.Drawing.Point(27, 137);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(131, 33);
+            this.btnProcess.TabIndex = 3;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Images Found";
+            // 
+            // txtFileSummary
+            // 
+            this.txtFileSummary.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFileSummary.Location = new System.Drawing.Point(108, 57);
+            this.txtFileSummary.Multiline = true;
+            this.txtFileSummary.Name = "txtFileSummary";
+            this.txtFileSummary.ReadOnly = true;
+            this.txtFileSummary.Size = new System.Drawing.Size(564, 69);
+            this.txtFileSummary.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(31, 176);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(641, 173);
+            this.textBox1.TabIndex = 6;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 361);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtFileSummary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnProcess);
+            this.Controls.Add(this.btnFindPath);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.MinimumSize = new System.Drawing.Size(700, 400);
+            this.Name = "frmMain";
+            this.Text = "Rename Images";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button btnFindPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFileSummary;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ImageRename/frmMain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ImageRename
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFindPath_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(txtPath.Text))
+            {
+                folderBrowserDialog1.SelectedPath = txtPath.Text;
+            }
+            folderBrowserDialog1.ShowNewFolderButton = true;
+
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtPath.Text = folderBrowserDialog1.SelectedPath.ToString();
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(Properties.Settings.Default.Path) &&
+                Directory.Exists(Properties.Settings.Default.Path))
+            {
+                txtPath.Text = Properties.Settings.Default.Path;
+            }
+        }
+
+    }
+}
