74b8b6c3915f4fc9b5e5f161b3f7efe854844e97
Bruce <bruce@bennettconsulting.eu>
2020-02-10 10:55:32 +0000

    Change IGPSCoridates to interface

--- ImageRename.Standard/Model/GPSCoridates.cs
+++ ImageRename.Standard/Model/GPSCoridates.cs
@@ -2,23 +2,23 @@
 
 namespace ImageRename.Standard.Model
 {
-    public class GPSCoridates: IGPSCoridates
+    public class GPSCoridates : IGPSCoridates
     {
+        public double DegreesLatitude { get { return Latitude.ToDegrees(); } }
+        public double DegreesLongitude { get { return Longitude.ToDegrees(); } }
         public DateTime? GpsDateTime { get; set; }
         public string Latitude { get; set; }
 
         public string Longitude { get; set; }
-        public double DegreesLongitude { get { return Longitude.ToDegrees(); } }
-        public double DegreesLatitude { get { return Latitude.ToDegrees(); } }
     }
 
-    public class IGPSCoridates
+    public interface IGPSCoridates
     {
-        DateTime? GpsDateTime { get; set; }
+         double DegreesLatitude { get; }
+         double DegreesLongitude { get; }
+         DateTime? GpsDateTime { get; set; }
          string Latitude { get; set; }
 
          string Longitude { get; set; }
-         double DegreesLongitude { get; }
-         double DegreesLatitude { get; }
     }
 }
